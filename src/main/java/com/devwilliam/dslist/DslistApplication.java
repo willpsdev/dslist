98dee9f93697e31cedb5c1678dfe1cc20b9873c8
William Santos <wpaixao.s2004@gmail.com>
2025-01-22 19:44:49 -0300

    Project created

---
+++ src/main/java/com/devwilliam/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.devwilliam.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devwilliam/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwilliam.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
